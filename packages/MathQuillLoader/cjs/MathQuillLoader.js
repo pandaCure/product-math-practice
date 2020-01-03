31c98b6c8fa0177d7255b0170ffc047db1b223d0
pandaCure <zzyamor@163.com>
2020-01-03 14:13:54 +0800

    fix load .scss error

--- packages/MathQuillLoader/cjs/MathQuillLoader.js
+++ packages/MathQuillLoader/cjs/MathQuillLoader.js
@@ -1,6 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./style/mathquill.scss");
+Object.defineProperty(exports, '__esModule', { value: true })
+require('../style/mathquill.scss')
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-exports.MathQuill = require('exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery!@edtr-io/mathquill/build/mathquill.js');
-exports.MQ = exports.MathQuill.getInterface(2);
+exports.MathQuill = require('exports-loader?window.MathQuill!imports-loader?window.jQuery=jquery!@edtr-io/mathquill/build/mathquill.js')
+exports.MQ = exports.MathQuill.getInterface(2)
